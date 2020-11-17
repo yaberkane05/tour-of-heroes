6ed5eb884efb18366343f08ccfcefee5b8ff5d4a
Younes Aberkane <y.aberkane@MBP085.lan>
2020-11-17 11:01:11 +0100

    display a list, create a feature component (step 2,3)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,9 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 @NgModule({
-  declarations: [AppComponent, HeroesComponent],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
